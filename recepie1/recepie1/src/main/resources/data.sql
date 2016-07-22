f058d447afff85bf35944bb50abd68ac481c1725
apandit <apandit@CAPMPT2040036.CAPMPTDC.COM>
2016-07-22 20:21:46 +0530

    Added new tables

---
+++ recepie1/recepie1/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into PUBLISHER(code, name, address) values ('001', 'Tata', 'India');
+insert into PUBLISHER(code, name, address) values ('002', 'Person', 'USA');
+insert into book(isbn, name, publishDate, price, publisherCode) values ('PBN001', 'Java complete Reference', DATE(2008-02-02), 300, '001');
+insert into book(isbn, name, publishDate, price, publisherCode) values ('PBN002', 'Computer Networks', DATE(2010-03-02), 200, '002');
